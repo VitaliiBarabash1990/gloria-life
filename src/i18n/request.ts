bde930058baaaf59a1e5c78188fb1e6911b9d889
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-09-08 21:59:26 +0200

    add request

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,22 +1,21 @@
-import {getRequestConfig} from 'next-intl/server';
-import {routing} from './routing';
+import { getRequestConfig } from "next-intl/server";
+import { routing } from "./routing";
 
-export default getRequestConfig(async ({requestLocale}) => {
-  // This typically corresponds to the `[locale]` segment
-  let locale = await requestLocale;
+type Locale = (typeof routing.locales)[number];
 
-  // Ensure that the incoming `locale` is valid
-  if (!locale || !routing.locales.includes(locale as any)) {
-    locale = routing.defaultLocale;
-  }
+export default getRequestConfig(async ({ requestLocale }) => {
+	let locale = (await requestLocale) as string;
 
-  return {
-    locale,
-    messages: (
-      await (locale === 'en'
-        ? // When using Turbopack, this will enable HMR for `en`
-          import('../../messages/en.json')
-        : import(`../../messages/${locale}.json`))
-    ).default
-  };
+	if (!locale || !routing.locales.includes(locale as Locale)) {
+		locale = routing.defaultLocale;
+	}
+
+	return {
+		locale,
+		messages: (
+			await (locale === "en"
+				? import("../../messages/en.json")
+				: import(`../../messages/${locale}.json`))
+		).default,
+	};
 });
