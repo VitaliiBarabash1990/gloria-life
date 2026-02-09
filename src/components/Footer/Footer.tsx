3c5b18ca71f1931bdbe4298f37a8ade956ac36c4
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2026-02-10 00:10:40 +0100

    add corect admin enter

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import WrapperForComponents from "../UI/WrapperForComponents/WrapperForComponents";
 import s from "./Footer.module.css";
 import { Link, usePathname, useRouter } from "@/i18n/routing";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "@/redux/store";
 import { logOut } from "@/redux/auth/operations";
+import { selectIsLoggedIn } from "@/redux/auth/selectors";
 
 const Footer = () => {
 	const dispatch = useDispatch<AppDispatch>();
 	const router = useRouter();
 	const path = usePathname().split("/");
+	const isLogin = useSelector(selectIsLoggedIn);
 	// console.log("Path", path[1]);
 
 	const hundlerRedirect = () => {
@@ -23,7 +25,9 @@ const Footer = () => {
 			<ul className={s.footerWrapper}>
 				{path[1] !== "admin" && (
 					<li className={`${s.topFooter} ${s.footerText}`}>
-						<Link href="/admin_authorization">Вхід для адміна</Link>
+						<Link href={isLogin ? "/admin" : "/admin_authorization"}>
+							{isLogin ? "Вхід в адмін панель" : "Вхід для адміна"}
+						</Link>
 					</li>
 				)}
 				{path[1] === "admin" && (
@@ -32,7 +36,7 @@ const Footer = () => {
 						className={`${s.topFooter} ${s.footerText} ${s.btnExit}`}
 						onClick={hundlerRedirect}
 					>
-						Вийти з режиму адмінісратора
+						Вийти з режиму адміністратора
 					</button>
 				)}
 				<li className={s.copyright}>
